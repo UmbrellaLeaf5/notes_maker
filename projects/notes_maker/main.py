eac8295c90c663d9481fba0a406e11dd54a67e27
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-05-20 18:13:04 +0300

    faster to finalize

--- projects/notes_maker/main.py
+++ projects/notes_maker/main.py
@@ -8,8 +8,9 @@ from note import NoteWidget
 class Window(QtWidgets.QMainWindow):
 
     __notes_list: list[NoteWidget]
-
+    __folder: str
     __untitled_counter: int
+    __current_index: int = 0
 
     def __init__(self) -> None:
         super().__init__()
@@ -19,25 +20,93 @@ class Window(QtWidgets.QMainWindow):
 
         self.__untitled_counter = 0
         self.__notes_list = []
+        self.__folder = ""
 
         self.ui.newPushButton.clicked.connect(self.NewNote)
+        self.ui.folderPushButton.clicked.connect(self.SelectFolder)
+        self.ui.notesListWidget.itemDoubleClicked.connect(self.OpenNote)
 
-    def NewNote(self) -> None:
+        self.ui.notesListWidget.itemSelectionChanged.connect(self.DeleteNoteBySelection)
+
+    def NewNote(self, action_stub, need_open: bool = True, title: str = "") -> None:
+        self.__current_index = -1
+
         self.__notes_list.append(NoteWidget())
-        self.__notes_list[-1].show()
 
-        self.__notes_list[-1].ui.deletePushButton.clicked.connect(self.DeleteNote)
+        self.__notes_list[self.__current_index].ui.deletePushButton.clicked.connect(self.DeleteNote)
 
-        if (self.__untitled_counter == 0):
-            self.ui.notesListWidget.addItem(f"UntitledNote")
-        else:
-            self.ui.notesListWidget.addItem(f"UntitledNote_{self.__untitled_counter}")
+        if (need_open):
+            self.__notes_list[self.__current_index].show()
+
+        if (title == ""):
+            title = "UntitledNote"
+
+            if (self.__untitled_counter):
+                title = f"UntitledNote_{self.__untitled_counter}"
+
+        self.ui.notesListWidget.addItem(title)
+        self.__notes_list[self.__current_index].ui.titleLineEdit.setText(title)
+
+        if (self.__folder == ""):
+            self.SelectFolder()
+
+        self.SaveNote()
 
         self.__untitled_counter += 1
 
-    def DeleteNote(self, index: int = -1) -> None:
-        self.__notes_list.pop(index)
-        self.ui.notesListWidget.takeItem(index)
+    def DeleteNote(self) -> None:
+        self.ui.notesListWidget.takeItem(self.__current_index)
+
+        import os
+        file = os.path.join(
+            self.__folder, self.__notes_list[self.__current_index].ui.titleLineEdit.text())
+        os.remove(file + ".txt")
+
+        self.__notes_list.pop(self.__current_index)
+
+    def OpenNote(self, item: QtWidgets.QListWidgetItem):
+        self.__current_index = self.ui.notesListWidget.row(item)
+        self.__notes_list[self.__current_index].show()
+
+        self.__notes_list[self.__current_index].ui.deletePushButton.clicked.connect(self.DeleteNote)
+        self.__notes_list[self.__current_index].ui.savePushButton.clicked.connect(self.SaveNote)
+
+    def SaveNote(self):
+        file_name = self.__notes_list[self.__current_index].ui.titleLineEdit.text()
+
+        import os
+        os.makedirs(self.__folder, exist_ok=True)
+
+        file_path = os.path.join(self.__folder, file_name)
+
+        if (file_path[-4:] != ".txt"):
+            file_path += ".txt"
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(self.__notes_list[self.__current_index].ui.textEdit.toPlainText())
+
+    def SelectFolder(self):
+        self.__folder = QtWidgets.QFileDialog.getExistingDirectory(
+            self, "Select folder", "")
+
+        if self.__folder:
+            self.UpdateNotesList()
+
+    def DeleteNoteBySelection(self, item: QtWidgets.QListWidgetItem):
+        self.__current_index = self.ui.notesListWidget.row(item)
+        self.DeleteNote()
+
+    def UpdateNotesList(self):
+        self.ui.notesListWidget.clear()
+        self.__notes_list.clear()
+
+        import os
+        files = os.listdir(self.__folder)
+
+        txt_files = [f for f in files if f.endswith('.txt')]
+
+        for txt_file in txt_files:
+            self.NewNote(0, False, txt_file[:-4])
 
 
 if __name__ == "__main__":
