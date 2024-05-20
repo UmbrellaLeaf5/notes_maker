9d9fbc42e9ad51f05cea42dae03b13508f33db04
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-05-20 16:53:00 +0300

    add primary DeleteNote feature

--- projects/notes_maker/main.py
+++ projects/notes_maker/main.py
@@ -6,17 +6,38 @@ from note import NoteWidget
 
 
 class Window(QtWidgets.QMainWindow):
+
+    __notes_list: list[NoteWidget]
+
+    __untitled_counter: int
+
     def __init__(self) -> None:
         super().__init__()
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.__untitled_counter = 0
+        self.__notes_list = []
+
         self.ui.newPushButton.clicked.connect(self.NewNote)
 
     def NewNote(self) -> None:
-        self.note = NoteWidget()
-        self.note.show()
+        self.__notes_list.append(NoteWidget())
+        self.__notes_list[-1].show()
+
+        self.__notes_list[-1].ui.deletePushButton.clicked.connect(self.DeleteNote)
+
+        if (self.__untitled_counter == 0):
+            self.ui.notesListWidget.addItem(f"UntitledNote")
+        else:
+            self.ui.notesListWidget.addItem(f"UntitledNote_{self.__untitled_counter}")
+
+        self.__untitled_counter += 1
+
+    def DeleteNote(self, index: int = -1) -> None:
+        self.__notes_list.pop(index)
+        self.ui.notesListWidget.takeItem(index)
 
 
 if __name__ == "__main__":
